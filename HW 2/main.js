30bdd3909fd795258d887d63d309ee5c6e355c6e
BlueSkytop <olgakutnja@gmail.com>
2023-08-19 14:01:43 +0300

    add 3 books

---
+++ HW 2/main.js
@@ -0,0 +1,22 @@
+let users = [
+    'Ostap', 'Maks', 'Lyuba', 'Muron', 'Ira', 'Nazar', 'Petro', 'Anya', 'Romana', 'Vika'
+];
+console.log(users);
+
+
+    let book_1 = {
+        name: 'History of Ukraine',
+        pageCount: 250,
+        genre: 'history'
+    }
+    let book_2 = {
+        name: 'Iron Flame',
+        pageCount: 640,
+        genre: 'adventures'
+    }
+    let book_3 = {
+        name: 'Fractal Noise: A Fractalverse Novel',
+        pageCount: 265,
+        genre: 'fantasy'
+    }
+console.log(book_1, book_2, book_3);
