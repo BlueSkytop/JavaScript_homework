ca7e61696efb38ccc8e606698121e4ef661a5c6c
BlueSkytop <olgakutnja@gmail.com>
2023-08-24 17:50:43 +0300

    add cycle status and age

--- HW 3/main.js
+++ HW 3/main.js
@@ -93,3 +93,38 @@ let products = [
 for (let product of products) {
     document.write(`<div class="product-card"> <h3 class="product-title">${product.title} - Price ${product.price}</h3> <img src="${product.image}" alt="" class="product-image"> </div>`)
 }
+
+//є масив
+//за допомоги циклу вивести:
+ //   - користувачів зі статусом true
+//- користувачів зі статусом false
+//- користувачів які старші за 30 років
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for(let user of users){
+    if(user.status){
+        document.write(`<div>${user.name} ${user.status} </div>`)
+    }
+}
+for(let user of users){
+    if(!user.status){
+        document.write(`<div>${user.name} ${user.status} </div>`)
+    }
+}
+for(let user of users){
+    if(user.age > 30){
+        document.write(`<div>${user.name} ${user.age} </div>`)
+    }
+}
+
