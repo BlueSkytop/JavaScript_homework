edd0fa8f21ef62a6ff90d445e6e5a8dd94e28521
BlueSkytop <olgakutnja@gmail.com>
2023-08-28 14:45:43 +0300

    add functions text

--- HW 4/main.js
+++ HW 4/main.js
@@ -5,11 +5,25 @@ function calc(a, b) {
 calc(a = 20, b = 10)
 
 //- створити функцію яка обчислює та повертає площу кола з радіусом r
-function calc2(p, a, r) {
-    let d  =  a * a
-    let result = p * d
-    console.log(result)
+function calc2(p, r) {
+    console.log(p * (r * r))
 }
-calc2(p = 3.14, a = 10)
+calc2(p = 3.14, r = 10)
 
 //- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+function cylinder(p, r, h) {
+    console.log((2 * p * r * h) + (2 * p * (r * r)))
+}
+cylinder(p = 3.14, r= 10, h = 50)
+
+//- створити функцію яка приймає масив та виводить кожен його елемент
+function foods() {
+    console.log(Array.from(arguments));
+}
+foods('popcorn', 'pizza', 'cake', 'spaghetti', 'sauce')
+
+//- створити функцію яка створює параграф з текстом. Текст задати через аргумент
+function text () {
+    console.log(arguments);
+}
+text(' Функції — це головні “будівельні блоки” програми.')
