c30b8b5d08820be9dee90af4dac64ebd87f587ab
BlueSkytop <olgakutnja@gmail.com>
2023-10-01 23:20:01 +0300

    add user-info and user-list

---
+++ HW 12/user-list/main.js
@@ -0,0 +1,19 @@
+const foo = async () => {
+    const  res = await fetch('http://jsonplaceholder.typicode.com/users');
+    const users = await  res.json();
+    const wrapper = document.getElementById('wrap');
+
+    for (const user of users) {
+        const div = document.createElement('div');
+        const button = document.createElement('button');
+        div.innerText = `#${user.id} ${user.name}`;
+        button.innerText = 'more';
+        div.appendChild(button);
+        wrapper.appendChild(div);
+
+        button.onclick = () => {
+            location.href = `.../user-info/index.html?userId=${user.id}`;
+        }
+    }
+}
+void foo();
