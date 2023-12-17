f796e1bb48facbee4939a2cc4db189b23cc879f1
BlueSkytop <olgakutnja@gmail.com>
2023-12-17 18:14:34 +0200

    corrected 3HW

--- HW 3/main.js
+++ HW 3/main.js
@@ -1,64 +1,105 @@
-let books = [
-    {name: 'A Thousand Splendid Suns', author: 'Khaled Hosseini', pages: 255},
-    {name: '9 листопада', author: 'Коллін Гувер', pages: 235},
-    {name: 'Кафе на краю світу', author: 'Джон П. Стрелекі', pages: 200},
-    {name: 'Велич і ницість. ', author: 'Ерік Ларсон', pages: 592},
-    {name: 'Вибір', author: 'Едіт Еґер', pages: 400},
-    {name: 'Нові Темні Віки. Колонія.', author: 'Макс Кідрук', pages: 904},
-    {name: 'Веріті', author: 'Коллін Гувер ', pages: 336},
-    {name: 'Наодинці з собою', author: 'Марк Аврелій', pages: 180},
-    {name: 'Ми, живі', author: 'Айн Ренд', pages: 504},
-    {name: 'Гімн', author: 'Айн Ренд ', pages: 112},
-];
-
- //   - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
-console.log(books.length);
-for (let i = 0; i < books.length; i++) {
-    const book = books[i];
-    document.write(`<div>${book.name} ${book.author} </div>`);
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>${'Назва товару'}</div>`);
 }
 
 //- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
-for (let i = 0; i < books.length; i++) {
-    const book = books[i];
-    document.write(`<div>${book.name} ${i}</div>`);
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>${i} ${'добрий вечір'} </div>`);
 }
 
-//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
-//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-let greetings = [
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-    {text:'guten tag'},
-];
-console.log(greetings.length);
-let i=0;
-while (i<greetings.length){
-    let greeting = greetings[i];
-    document.write(`<h1>${greeting.text} ${i}</h1>`);
+//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині
+let  blocks = [
+    {text:'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+    {text: 'hello'},
+]
+let i = 0;
+while (i<blocks.length) {
+    let block = blocks[i];
+    document.write(`<h1>${block.text}</h1>`);
     i++;
 }
 
+//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+let blocks2 = [
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+    {text:'the best times'},
+]
+let l = 0;
+while (l<blocks2.length) {
+    let block2 = blocks2[l];
+    document.write(`<h1>${block2.text} ${l}</h1>`);
+    l++;
+}
 
-//Використовуючи данні з масиву, за допомоги document.write та циклу
-//побудувати структуру по шаблону
+
+/*- Використовуючи данні з масиву, за допомоги document.write та циклу
+побудувати структуру по шаблону
+
+ШАБЛОН:
+    <ul>
+        <li>ITEM OF ARRAY</li>
+        <!--
+            і тд інші об'єкти масиву
+             ...
+             ...
+             ...
+        -->
+    </ul>
+
+замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву*/
+let listOfItems = ['html','css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+for (let j = 0; j < listOfItems.length; j++) {
+    const listOfItem = listOfItems[j];
+    document.write(`<ul><li>${listOfItem}</li></ul>`)
+}
+
+/*  Використовуючи данні з масиву, за допомоги document.write та циклу
+побудувати структуру по шаблону
+Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+
+ШАБЛОН
+<div class="product-card">
+  <h3 class="product-title">TITLE. Price - PRICE</h3>
+<img src="IMAGE" alt="" class="product-image">
+</div>
+Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту*/
 let products = [
     {
         title: 'milk',
@@ -81,15 +122,17 @@ let products = [
         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
     },
 ];
-for (let product of products) {
-    document.write(`<div class="product-card"> <h3 class="product-title">${product.title} - Price ${product.price}</h3> <img src="${product.image}" alt="" class="product-image"> </div>`)
+for (const product of products) {
+    document.write(`<div class="product-card">
+    <h3 class="product-title">${product.title} - Price ${product.price}</h3><img src="${product.image}" alt="image" class="product-image"></div>`)
 }
 
-//є масив
-//за допомоги циклу вивести:
- //   - користувачів зі статусом true
-//- користувачів зі статусом false
-//- користувачів які старші за 30 років
+/*--------------------
+    є масив
+за допомоги циклу вивести:
+    - користувачів зі статусом true
+- користувачів зі статусом false
+- користувачів які старші за 30 років */
 let users = [
     {name: 'vasya', age: 31, status: false},
     {name: 'petya', age: 30, status: true},
@@ -103,27 +146,19 @@ let users = [
     {name: 'olya', age: 31, status: false},
     {name: 'max', age: 31, status: true}
 ];
-for(let user of users){
-    if(user.status){
-        document.write(`<div>${user.name} ${user.status} </div>`)
-    }
-}
-for(let user of users){
-    if(!user.status){
-        document.write(`<div>${user.name} ${user.status} </div>`)
-    }
-}
-for(let user of users){
-    if(user.age > 30){
-        document.write(`<div>${user.name} ${user.age} </div>`)
-    }
-}
 
-//- Використовуючи данні з масиву, за допомоги document.write та циклу
-//побудувати структуру по шаблону
-let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-for (let j = 0; j < listOfItems.length; j++) {
-    const listOfItem = listOfItems[j];
-    document.write(`<ul> <li>${listOfItem}</li>
-</ul>`)
+for (const user of users) {
+    if (user.status) {
+        document.write(`<div>${user.name} ${user.status}</div>`)
+    }
+}
+for (const user of users) {
+    if (!user.status) {
+        document.write(`<div>${user.name} ${user.status}</div>`)
+    }
+}
+for (const user of users) {
+    if (user.age>=30) {
+        document.write(`<div>${user.name} ${user.age}</div>`)
+    }
 }
